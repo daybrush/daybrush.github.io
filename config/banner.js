a97141f9f29b007454816e30150f5e88aacde5d1
Daybrush <younkue.choi@navercorp.com>
2023-05-20 16:47:48 +0900

    chore: remove all files

--- config/banner.js
+++
@@ -1,6 +0,0 @@
-const pkg = require("../package.json");
-module.exports = `/*
-Copyright (c) 2018 ${pkg.author}
-Dependencies : ${Object.keys(pkg.dependencies).join(", ")}
-Homepage: https://daybrush.com
-*/`;
